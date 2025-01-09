9752cf818d88cb64386c79af78e241b984963179
ochamu <171050612+ochamu@users.noreply.github.com>
2025-01-09 14:21:08 +0900

    マークを絵文字に変更

--- Card.java
+++ Card.java
@@ -1,7 +1,7 @@
 public class Card {
   int number;
   String mark;
-  String[] marks = {"ハート", "ダイヤ", "クローバー", "スペード"};
+  String[] marks = {"♥", "♦", "♣", "♠"};
   void open() {
     number = (int)(Math.random() * 13) + 1;
     mark = marks[(int)(Math.random() * 4)];
