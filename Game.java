f25f0d442cf9ba1878ac5dd0f8947a16cfaa688b
ochamu <171050612+ochamu@users.noreply.github.com>
2025-01-30 14:00:51 +0900

    2枚目に引いたカードとの数の大小を予想するゲームに変更

--- Game.java
+++ Game.java
@@ -18,18 +18,23 @@ public class Game {
   }
 
   void start() {
-    Card c = p.draw(cards);
-    System.out.println("Playerが引いたカード: " + c.mark + "の" + c.number + "\n");
-    judge(c);
+    Card c1 = p.draw(cards);
+    System.out.println("開いたカード: " + c1.mark + "の" + c1.number + "\n");
+    Card c2 = p.draw(cards);
+    System.out.println("もう一枚カードを伏せました。");
+    // System.out.println("伏せたカード: " + c2.mark + "の" + c2.number + "\n");
+    String ans = p.answer();
+    judge(c1, c2, ans);
   }
 
-  void judge(Card c) {
-    if (c.number >= 10) {
+  void judge(Card c1, Card c2, String ans) {
+    c2.open();
+    if ((ans.equals("h") && c1.number < c2.number) || (ans.equals("l") && c1.number > c2.number)) {
       System.out.println("勝ち");
-    }else {
+    } else if (c1.number==c2.number) {
+      System.out.println("引き分け");
+    } else {
       System.out.println("負け");
     }
   }
 }
-
-
--- Player.java
+++ Player.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Player {
 
   Card draw(Card[] cards) {
     int rnd = (int)(Math.random()*cards.length);
     return cards[rnd];
+  }
+  
+  String answer() {
+    System.out.print("High(h) or Low(l)?");
+    Scanner scan = new Scanner(System.in);
+    return scan.nextLine();
   }
 }
